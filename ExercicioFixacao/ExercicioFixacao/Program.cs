d8c4004431407ccaaef8afe04a28c605b22264ba
Ricardo Alves <dev.rical21@gmail.com>
2021-08-10 16:04:01 -0300

    Commit Curso C# 10/08/2021

---
+++ ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] dados = Console.ReadLine().Split(' ');
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(dados[0]);
+            Console.WriteLine(dados[1]);
+            Console.WriteLine(dados[2]);
+
+        }
+    }
+}
--- Primeiro/Primeiro/Program.cs
+++ Primeiro/Primeiro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Primeiro
 {
@@ -6,10 +7,31 @@ namespace Primeiro
     {
         static void Main(string[] args)
         {
-            int a = 5;
-            int b = 2;
-            double resultado = (double) a / b;
-            Console.WriteLine(resultado);
+            // Console.WriteLine("Qual frase irá digitar? ");
+            // string s = Console.ReadLine();
+            // string[] vet = Console.ReadLine().Split(' ');
+            // string p1 = vet[0];
+            // string p2 = vet[1];
+            // string p3 = vet[2];
+            // Console.WriteLine("As palavras digitadas foram: " + p1 + " e " + p2 + " e " + p3);
+
+            Console.WriteLine("Qual frase irá digitar? ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
         }
     }
 }
+ 
