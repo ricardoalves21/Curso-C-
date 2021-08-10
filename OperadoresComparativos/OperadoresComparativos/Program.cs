359f09d5963aa73d2117fb75a3667759718b5c98
Ricardo Alves <dev.rical21@gmail.com>
2021-08-10 17:41:43 -0300

    Commit curso de C# 10/08/2021

---
+++ OperadoresComparativos/OperadoresComparativos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OperadoresComparativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            bool c1 = a < 10;
+            Console.WriteLine(c1);
+        }
+    }
+}
