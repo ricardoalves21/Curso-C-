2c4d20727266b42befa8d5a43a5092ba54953f2a
Ricardo Alves <dev.rical21@gmail.com>
2021-08-10 19:58:34 -0300

    Commit Curso C# 10/08/2021

---
+++ EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("oi");
+            }
+        }
+    }
+}
