91518fcf5eb1b7f4e8bd1d47ea2f084afb53ee8c
Ricardo Alves <dev.rical21@gmail.com>
2021-08-14 09:09:32 -0300

    Commit curso C# 14/08/2021

--- Triangulo/Triangulo/Program.cs
+++ Triangulo/Triangulo/Program.cs
@@ -6,27 +6,24 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
-            Triangulo x = new Triangulo();
-            Triangulo y = new Triangulo();
+            Triangulo x = new Triangulo();  // instanciando o objeto x que será originado da classe Triangulo
+            Triangulo y = new Triangulo();  // instanciando o objeto y que será originado da classe Triangulo
 
             Console.WriteLine("Entre com as medidas do triângulo X:");
-            x.A = double.Parse(Console.ReadLine());
-            x.B = double.Parse(Console.ReadLine());
-            x.C = double.Parse(Console.ReadLine());
+            x.A = double.Parse(Console.ReadLine());  // dando ao objeto x o atributo A
+            x.B = double.Parse(Console.ReadLine());  // dando ao objeto x o atributo B
+            x.C = double.Parse(Console.ReadLine());  // dando ao objeto x o atributo C
 
             Console.WriteLine("Entre com as medidas do triângulo Y:");
-            y.A = double.Parse(Console.ReadLine());
-            y.B = double.Parse(Console.ReadLine());
-            y.C = double.Parse(Console.ReadLine());
+            y.A = double.Parse(Console.ReadLine());  // dando ao objeto y o atributo A
+            y.B = double.Parse(Console.ReadLine());  // dando ao objeto y o atributo B
+            y.C = double.Parse(Console.ReadLine());  // dando ao objeto y o atributo C
 
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            double areaX = x.Area();  // dando ao objeto x o método Area()
+            double areaY = y.Area();  // dando ao objeto y o método Area()
 
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-
-            Console.WriteLine("Área de X = " + areaX.ToString("F4"));
-            Console.WriteLine("Área de x = " + areaY.ToString("F4"));
+            Console.WriteLine("Área de X = " + areaX.ToString("F4"));  // apresentando a variável areaX no formato com 4 casas decimais
+            Console.WriteLine("Área de x = " + areaY.ToString("F4"));  // apresentando a variável areaY no formato com 4 casas decimais
 
             if (areaX > areaY)
             {
--- Triangulo/Triangulo/Triangulo.cs
+++ Triangulo/Triangulo/Triangulo.cs
@@ -1,11 +1,19 @@
 ﻿
-namespace Triangulo
+using System;  // Importação de dependências
+
+namespace Triangulo  // Namespace da classe
 {
-    class Triangulo
+    class Triangulo  // Nome da classe
     {
         // ATRIBUTOS
         public double A;
         public double B;
         public double C;
+
+        public double Area()  // se o método não retorna nada, usa-se a palavra "void"
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
