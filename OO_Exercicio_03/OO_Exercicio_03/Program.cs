b5b5ed93e71872f36189aa8a16004f8fc45455a1
ricardoalves21 <dev.rical21@gmail.com>
2021-08-17 21:33:09 -0300

    Teste 17/08/2021

---
+++ OO_Exercicio_03/OO_Exercicio_03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OO_Exercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
