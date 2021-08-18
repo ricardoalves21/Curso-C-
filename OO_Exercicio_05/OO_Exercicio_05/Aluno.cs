727af8ae91f0a150f9ea091633dcc2994f6c75f2
ricardoalves21 <dev.rical21@gmail.com>
2021-08-18 11:15:08 -0300

    Commit curso C# OO_Exercicio_05 18/08/2021

---
+++ OO_Exercicio_05/OO_Exercicio_05/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_Exercicio_05
+{
+    class Aluno
+    {
+        // ATRIBUTOS
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        // MÉTODOS
+        public double NotaFinal()
+        {
+            double total = Nota1 + Nota2 + Nota3;
+            return total;
+        }
+    }
+}
