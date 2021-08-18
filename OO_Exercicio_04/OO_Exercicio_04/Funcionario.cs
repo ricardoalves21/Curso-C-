89a42afd88b49bbaecf1475812b1cfba5ac20d7c
ricardoalves21 <dev.rical21@gmail.com>
2021-08-18 08:13:00 -0300

    Commit OO_Exercicio_04 18/08/2021

---
+++ OO_Exercicio_04/OO_Exercicio_04/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_Exercicio_04
+{
+    class Funcionario
+    {
+        // ATRIBUTOS
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        // MÉTODOS
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario (double porcentagem)
+        {
+            porcentagem = porcentagem / 100;
+            SalarioBruto += SalarioBruto * porcentagem; 
+        }
+        public override string ToString()
+        {
+            return
+                Nome
+                + ", $ "
+                + SalarioLiquido().ToString("F2");
+        }
+    }
+}
