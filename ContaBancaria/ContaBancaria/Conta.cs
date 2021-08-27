84a38f8c9f05026d665266f0a6fa571023bd6e14
ricardoalves21 <ricardo.alves@netimoveis.com>
2021-08-27 18:07:24 -0300

    Curso Csharp 27/08/2021

---
+++ ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        // ATRIBUTOS
+        private int Numero { get; set; }
+        private int Titular { get; set; }
+        private int _saldo;
+
+        // CONSTRUTOR
+        public Conta(int numero, int titular, int saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            _saldo = saldo;
+        }
+
+        public Conta()
+        {
+        }
+
+        // MÉTODOS
+        public int Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                _saldo += Saldo;
+            }
+        }
+
+        override
+        public string ToString()
+        {
+            return
+                "Conta número: "
+                + Numero
+                + "\n"
+                + "Titular: "
+                + Titular
+                + "\n"
+                + "Saldo: "
+                + _saldo.ToString("F2")
+                + "\n";
+        }
+        
+
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            Console.WriteLine("Digite o número da conta: ");
+            conta.
+
+        }
+    }
+}
