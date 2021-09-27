38c3908de0af5d205a9859329481c69b19dd4642
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-09-27 16:02:40 -0300

    Curso C# 27/09/2021

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace FuncaoData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2021, 09, 27); // apresenta a data do parâmetro '27/09/2021 00:00:00'
+            DateTime d2 = new DateTime(2021, 09, 27, 20, 45, 3); // apresenta a data/hora do parâmetro '27/09/2021 20:45:03'
+            DateTime d3 = new DateTime(2021, 09, 27, 20, 45, 3, 500); // apresenta a data/hora do parâmetro com milesegundos '27/09/2021 20:45:03' 
+            DateTime d4 = DateTime.Now; // apresenta a data/hora do sistema
+            DateTime d5 = DateTime.Today; // apresenta a data do dia atual às zero horas (00:00:00)
+            DateTime d6 = DateTime.UtcNow; // apresenta a data/hora atual em Greenwich
+            DateTime d7 = DateTime.Parse("2021-09-27"); // converte a string apontada no parâmetro para o formato data/hora
+            DateTime d8 = DateTime.Parse("2021-09-27 15:30:15"); // converte a string apontada no parâmetro para o formato data/hora
+            DateTime d9 = DateTime.Parse("27/09/2021"); // converte a string apontada no parâmetro para o formato data/hora do Brasil
+            DateTime d10 = DateTime.Parse("27/09/2021 16:45:10"); // converte a string apontada no parâmetro para o formato data/hora do Brasil
+            DateTime d11 = DateTime.ParseExact("2021-09-27", "yyyy-MM-dd", CultureInfo.InvariantCulture); // para determinar o formato de data que quero utilizar
+            DateTime d12 = DateTime.ParseExact("27/09/2021 13:05:55", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture); // para determinar o formato de data/hora que quero utilizar
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
+        }
+    }
+}
---
+++ FuncoesString/FuncoesString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FuncoesString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG     ";
+
+            string s1 = original.ToUpper(); // transforma tudo em maiúscula
+            string s2 = original.ToLower(); // transforma tudo em minúscula
+            string s3 = original.Trim(); // retira os espaços antes e depois da string
+            int n1 = original.IndexOf("bc"); // busca a primeira ocorrência de "bc" dentro da string
+            int n2 = original.LastIndexOf("bc"); // busca a última ocorrência de "bc" dentro da string
+            string s4 = original.Substring(6, 2); // recorta uma string a partir de um índice e da quantidade de posições a serem recortadas dessa string
+            string s5 = original.Replace('a', 'x'); // substitui a letra 'a' pela letra 'x' em uma string
+            string s6 = original.Replace("bcd", "kw"); // substitui a letra 'a' pela letra 'x' em uma string
+            bool b1 = String.IsNullOrEmpty(original); // avalia se a variável é nula (NULL) ou vazia (EMPTY);
+            bool b2 = String.IsNullOrWhiteSpace(original); // avalia se a variável é nula ou é espaço em branco
+
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring('bc'): " + s4 + "-");
+            Console.WriteLine("Replace('a', 'x'): " + s5 + "-");
+            Console.WriteLine("Replace('bcd', 'xy'): " + s6 + "-");
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+        }
+    }
+}
