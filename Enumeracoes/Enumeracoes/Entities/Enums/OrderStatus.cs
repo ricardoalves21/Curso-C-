dbc8076ffbccdf9af351c6c6f914ac6401630712
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-09-30 08:21:11 -0300

    Curso C# - Enumerações 30/09/2021

---
+++ Enumeracoes/Enumeracoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enumeracoes.Entities.Enums
+{
+    enum OrderStatus : int  // classe do tipo ENUM que está herdando atributos e métodos da classe 'int'
+    {
+        PendingPayment = 0,
+        Processing = 1, 
+        Shipper = 2, 
+        Delivered = 3
+    }
+}
---
+++ Enumeracoes/Enumeracoes/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes.Entities
+{
+    class Order
+    {
+        // ATRIBUTOS
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        // MÉTODOS
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
