bbac2199ce5c312a4acdc8627a9f5de53089291b
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-10-21 07:15:40 -0300

    Curso de C# - exercicio composição

---
+++ ExercicioEnumComposicao/ExercicioEnumComposicao/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioEnumComposicao
+{
+    class Order
+    {
+        // atributos
+        public  DateTime Moment{ get; set; }
+        public OrderStatus Status { get; set; } // composição simples (relação tem um)
+        public Client Client { get; set; } // composição simples (relação tem um)
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>(); // composição lista (relação tem muitos)
+
+        // construtores
+        public Order(DateTime moment, OrderStatus status)
+        {
+            Moment = moment;
+            Status = status;
+        }
+
+        public Order()
+        {
+        }
+
+        // método adiciona item
+        public void AddItem (OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        // método remove item
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        // método totaliza pedido
+        public double totalPedido()
+        {
+            double sum = 0;
+            foreach(OrderItem items in Items)
+            {
+                sum += items.subTotal();
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ ExercicioEnumComposicao/ExercicioEnumComposicao/Product.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ExercicioEnumComposicao
+{
+    class Product
+    {
+        // atributos
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // construtores
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product()
+        {
+        }
+    }
+}
