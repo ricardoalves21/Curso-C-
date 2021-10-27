725e63872fe34df178d407d0fdac25fc4904230f
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-10-26 23:16:37 -0300

    Curso C# 26/10/2021

---
+++ ExercicioHerancaPolimorfismo/ExercicioHerancaPolimorfismo/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioHerancaPolimorfismo.Entities
+{
+    class Product
+    {
+        // atributos
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // construtor
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public Product()
+        {
+        }
+
+        // métodos
+        public virtual string priceTag()
+        {
+            return Price.ToString();
+        }
+
+
+
+
+    }
+}
---
+++ ExercicioHerancaPolimorfismo/ExercicioHerancaPolimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioHerancaPolimorfismo.Entities
+{
+    class UsedProduct : Product
+    {
+        //atributos
+        public DateTime ManufacturedDate { get; set; }
+
+        public UsedProduct(string name, double price, DateTime manufacturedDate) : base(name, price)
+        {
+            ManufacturedDate = manufacturedDate;
+        }
+        public UsedProduct()
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
