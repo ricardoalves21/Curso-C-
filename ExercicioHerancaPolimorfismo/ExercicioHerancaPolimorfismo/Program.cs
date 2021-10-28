821c7e2310d7d99f2d55ea8d724aa1bda0f62620
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-10-28 08:47:41 -0300

    Curso C# Exercicio de POO 28/10/2021

--- ExercicioHerancaPolimorfismo/ExercicioHerancaPolimorfismo/Program.cs
+++ ExercicioHerancaPolimorfismo/ExercicioHerancaPolimorfismo/Program.cs
@@ -6,7 +6,30 @@ namespace ExercicioHerancaPolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number os products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++ )
+            {
+                Console.WriteLine($"Product #{i} data:"); // usando interpolação
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                
+                if (type.Equals('i')) // comparando a variável do tipo 'char'
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                } 
+                else if (type.Equals('u'))
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                }
+            }
         }
     }
 }
