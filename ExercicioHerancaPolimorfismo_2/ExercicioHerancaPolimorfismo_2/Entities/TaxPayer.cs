b184a274b6fa6b168f1fcabaac3add6218478419
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-11-04 10:11:14 -0300

    Curso de C# Exercicio de fixação de Herança e Polimorfismo 04/11/2021

---
+++ ExercicioHerancaPolimorfismo_2/ExercicioHerancaPolimorfismo_2/Entities/TaxPayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioHerancaPolimorfismo_2.Entities
+{
+    abstract class TaxPayer // super classe abstrata
+    {
+        //atributos da super classe
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        //construtor da super classe
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        //método abstrato da super classe
+        public abstract double tax();
+
+    }
+}
---
+++ ExercicioHerancaPolimorfismo_2/ExercicioHerancaPolimorfismo_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using ExercicioHerancaPolimorfismo_2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioHerancaPolimorfismo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>(); // lista que guardará todos os dados das três classes
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) // looping para incluir todos os contribuintes
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)?");
+                char type = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // este if é para sabermos na lista de qual classe incluiremos o contribuinte
+                if (type == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else if (type == 'c')
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            Console.WriteLine();
+
+            // este foreach é para varrermos a lista por completo a fim de apresentarmos todos os contribuintes que foram incluídos na lista
+            foreach (TaxPayer tp in list)
+            {
+                Console.WriteLine(tp.Name + ": $ " + tp.tax().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            double sum = 0; // variável global para acumular os valores dos impostos de todos os contribuintes
+
+            // este foreach é para varrer a lista inteira a fim de somar todos os impostos inseridos nela
+            foreach (TaxPayer tp in list)
+            {
+                sum += tp.tax();
+            }
+
+            // apresentar o total de impostos pagos por todos os contribuintes
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
