d74621ac1a60beedd305564e6fb71daf99c08530
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-11-28 15:35:28 -0300

    Curso C# Exercicio de fixação de Interface 28/11/2021

---
+++ ExercicioFixacaoInterface/ExercicioFixacaoInterface/Entities/Contract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioFixacaoInterface.Entities;
+using ExercicioFixacaoInterface.Service;
+
+namespace ExercicioFixacaoInterface.Entities
+{
+    class Contract
+    {
+
+        // atributos simples
+        private int Number { get; set; } // número do contrato
+        private DateTime Date { get; set; } // data do contrato
+        private double TotalValue { get; set; } // valor do contrato
+
+
+
+        // composição simples
+        public ContractService contractService { get; set; } // ligando as classes 'Contract' e 'ContractService'
+
+
+
+        // composição lista (atributo)
+        public List<Installment> installments { get; set; } = new List<Installment>(); // composição lista ( para guardar as parcelas do contrato)
+
+
+
+        // construtor
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }   
+        
+
+
+
+    }
+}
---
+++ ExercicioFixacaoInterface/ExercicioFixacaoInterface/Service/ContractService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioFixacaoInterface.Entities;
+
+namespace ExercicioFixacaoInterface.Service
+{
+    class ContractService
+    {
+        
+        public void processContract(Contract contract, int months)
+        {
+
+        }
+    }
+}
