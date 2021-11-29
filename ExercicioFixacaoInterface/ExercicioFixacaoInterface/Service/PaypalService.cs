715b2a202a5234a85fa93178df01a6f082423fb0
netimoveis-ricardo <ricardo.alves@netimoveis.com>
2021-11-29 10:16:55 -0300

    Curso C# Exercicio de fixação Interface 29/11/2021

--- ExercicioFixacaoInterface/ExercicioFixacaoInterface/Service/PaypalService.cs
+++ ExercicioFixacaoInterface/ExercicioFixacaoInterface/Service/PaypalService.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using ExercicioFixacaoInterface.Service;
 
-namespace ExercicioFixacaoInterface.Service
-{
-    class PaypalService : IOnlinePaymentService
-    {
-        public double paymentFee(double amount)
-        {
-            return 
-        }
+//namespace ExercicioFixacaoInterface.Service
+//{
+//    class PaypalService : IOnlinePaymentService
+//    {
+//        public double paymentFee(double amount)
+//        {
+//            return 
+//        }
 
-        public double interest(double amount, int months)
-        {
+//        public double interest(double amount, int months)
+//        {
 
-        }
+//        }
 
 
-    }
-}
+//    }
+//}
